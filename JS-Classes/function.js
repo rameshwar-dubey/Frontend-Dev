1f2a3d945a6aa750f66ac66ad2b5a2969e0a6aad
Rameshwar Dubey <rameshward040@gmail.com>
2025-11-26 11:08:33 +0530

    Delete JS-Classes directory

--- JS-Classes/function.js
+++
@@ -1,4 +0,0 @@
-function validateName(name){ return /^[A-Za-z ]+$/.test(name); }
-function validateEmail(email){ return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email); }
-function validatePhone(phone){ return /^\d{10}$/.test(phone); }
-function validatePassword(pass){ return /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{1,}$/.test(pass); }
