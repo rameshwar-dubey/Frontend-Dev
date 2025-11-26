85e162097513443970e62f851bfbbe7668a46f46
Rameshwar Dubey <rameshward040@gmail.com>
2025-11-26 11:09:14 +0530

    Delete JS-Operators directory

--- JS-Operators/stringManipulationReport.js
+++
@@ -1,4 +0,0 @@
-let product = " wireless headphones PRO ";
-let cleaned = product.trim().toLowerCase().split(" ").map(w=> w[0].toUpperCase()+ w.slice(1)).join(" ").replace("Pro","Pro Edition");
-console.log(cleaned);
-console.log("Length:", cleaned.length);
